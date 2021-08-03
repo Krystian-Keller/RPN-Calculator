17561e777f23e5fa368554b77491205d72052c5d
Krystian-Keller <krystiankeller2@gmail.com>
2021-08-03 16:24:26 -0300

    Fixed a mistake with the files

---
+++ RPN_Calculator/mainwindow.cpp
@@ -0,0 +1,261 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    count = 0;
+    aux = 0;
+
+    ui->textEdit->setReadOnly(true);
+    ui->lineEdit->setReadOnly(true);
+    symbol = "<img src=\":///resource/IndicativeArrow-img.png\" width=\"11\" height=\"11\" >";
+    ui->Arrow_Up->setIcon(QIcon(":/resource/UpArrow-img.png"));
+    ui->Arrow_Down->setIcon(QIcon(":/resource/DownArrow-img.png"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_Number_1_clicked()
+{
+    ui->lineEdit->insert("1");
+}
+
+
+void MainWindow::on_Number_2_clicked()
+{
+    ui->lineEdit->insert("2");
+}
+
+
+void MainWindow::on_Number_3_clicked()
+{
+    ui->lineEdit->insert("3");
+}
+
+
+void MainWindow::on_Number_4_clicked()
+{
+    ui->lineEdit->insert("4");
+}
+
+
+void MainWindow::on_Number_5_clicked()
+{
+    ui->lineEdit->insert("5");
+}
+
+
+void MainWindow::on_Number_6_clicked()
+{
+    ui->lineEdit->insert("6");
+}
+
+
+void MainWindow::on_Number_7_clicked()
+{
+    ui->lineEdit->insert("7");
+}
+
+
+void MainWindow::on_Number_8_clicked()
+{
+    ui->lineEdit->insert("8");
+}
+
+
+void MainWindow::on_Number_9_clicked()
+{
+    ui->lineEdit->insert("9");
+}
+
+
+void MainWindow::on_Number_0_clicked()
+{
+    ui->lineEdit->insert("0");
+}
+
+
+void MainWindow::on_Enter_Button_clicked()
+{
+    Numbers.push(ui->lineEdit->text().toDouble());
+
+    count++;
+    aux = 0;
+
+    Print();
+    ui->lineEdit->clear();
+
+}
+
+
+void MainWindow::on_Clear_Button_clicked()
+{
+    ui->lineEdit->clear();
+}
+
+void MainWindow::on_Del_Button_clicked()
+{
+    ui->textEdit->clear();
+    Numbers.clear();
+    count = 0;
+    aux = 0;
+}
+
+
+void MainWindow::on_Op_Addiction_clicked()
+{
+    if(Verification()){
+
+    Numbers.push(Numbers.pop() + Numbers.pop());
+
+    count--;
+
+    Print();
+    }
+
+}
+
+
+void MainWindow::on_Op_Multiplication_clicked()
+{
+    if(Verification()){
+
+    Numbers.push(Numbers.pop() * Numbers.pop());
+
+    count--;
+
+    Print();
+    }
+
+}
+
+
+void MainWindow::on_Op_Subtraction_clicked()
+{
+    if(Verification()){
+
+        int aux_1, aux_2;
+        aux_1 = Numbers.pop();
+        aux_2 = Numbers.pop();
+
+        Numbers.push(aux_2 - aux_1);
+
+        count--;
+
+        Print();
+    }
+}
+
+
+void MainWindow::on_Op_Division_clicked()
+{
+    if(Verification()){
+
+        int aux_1, aux_2;
+        aux_1 = Numbers.pop();
+        aux_2 = Numbers.pop();
+
+        if(aux_1 == 0){
+            ui->textEdit->clear();
+            ui->textEdit->insertPlainText("Math Error!  Invalid Division by Zero!");
+            ui->textEdit->insertPlainText("\nPress Del to continue.");
+        }else{
+            Numbers.push(aux_2 / aux_1);
+
+            count--;
+
+            Print();
+        }
+
+    }
+
+}
+void MainWindow::on_Rol_Button_clicked()
+{
+    if(Verification()){
+
+    int aux_1, aux_2;
+
+    aux_1 = Numbers.pop();
+    aux_2 = Numbers.pop();
+
+    Numbers.push(aux_1);
+    Numbers.push(aux_2);
+
+    Print();
+    }
+}
+
+
+void MainWindow::on_Arrow_Up_clicked()
+{
+
+
+    if(aux<(Numbers.size()-1)){
+        aux++;
+        Print();
+
+        int i;
+        for(i=0; i<=aux; i++){
+            ui->textEdit->moveCursor(QTextCursor::Up);
+        }
+    }
+}
+
+
+void MainWindow::on_Arrow_Down_clicked()
+{
+    if(aux>0){
+        aux--;
+        Print();
+
+        int i;
+        for(i=0; i<=(aux); i++){
+            ui->textEdit->moveCursor(QTextCursor::Up);
+        }
+    }
+
+}
+
+void MainWindow::Print()
+{
+    ui->textEdit->clear();
+    ui->textEdit->setAlignment(Qt::AlignRight);
+
+    int i, j=count;
+
+    for(i=0; i<j; i++){
+        if(((j-aux)-i)==1){
+            ui->textEdit->insertPlainText("   ");
+            ui->textEdit->insertHtml(symbol);
+            ui->textEdit->insertPlainText("   ");
+            ui->textEdit->insertPlainText(QString("%1").arg(Numbers.at(i)));
+            ui->textEdit->insertPlainText("\n");
+
+        }else{
+        ui->textEdit->insertPlainText(QString("%1").arg(Numbers.at(i)));
+        ui->textEdit->insertPlainText("\n");
+        }
+    }
+    ui->textEdit->moveCursor(QTextCursor::Down);
+}
+
+int MainWindow::Verification()
+{
+    if(Numbers.size()<2){
+        ui->textEdit->clear();
+        ui->textEdit->insertPlainText("Invalid Operation!");
+        ui->textEdit->insertPlainText("\nPress Del to continue.");
+        return 0;
+    }else{
+        return 1;
+    }
+
+}
---
+++ RPN_Calculator/mainwindow.h
@@ -0,0 +1,70 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStack>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_Number_1_clicked();
+
+    void on_Number_2_clicked();
+
+    void on_Number_3_clicked();
+
+    void on_Number_4_clicked();
+
+    void on_Number_5_clicked();
+
+    void on_Number_6_clicked();
+
+    void on_Number_7_clicked();
+
+    void on_Number_8_clicked();
+
+    void on_Number_9_clicked();
+
+    void on_Number_0_clicked();
+
+    void on_Enter_Button_clicked();
+
+    void on_Clear_Button_clicked();
+
+    void on_Del_Button_clicked();
+
+    void on_Op_Addiction_clicked();
+
+    void on_Op_Multiplication_clicked();
+
+    void on_Op_Subtraction_clicked();
+
+    void on_Op_Division_clicked();
+
+    void on_Rol_Button_clicked();
+
+    void on_Arrow_Up_clicked();
+
+    void on_Arrow_Down_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QStack<double> Numbers;
+    int count;
+    void Print();
+    int Verification();
+    int aux;
+    QString symbol;
+
+};
+#endif // MAINWINDOW_H
